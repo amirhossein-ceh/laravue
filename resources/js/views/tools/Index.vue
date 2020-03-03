607b2e212a83fe77556de1534af1b0647886d4b5
amirhossein <amirhossein.dedsec@gmail.com>
2020-03-03 12:47:53 -0800

    add tools to project

---
+++ resources/js/views/tools/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h1>Cache your view's to have better performance</h1>
+    <button id="btn" type="button" class="clear-view-cache-button" @click="viewCache()">Cache !</button>
+    <h3 id="msgView" />
+    <hr>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Tools',
+  methods: {
+    viewCache() {
+      axios.get('/api/view-cache')
+        .then(({ data }) => {
+          var msg = document.getElementById('msgView');
+          msg.innerText = data.text;
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .container {
+    text-align: center;
+  }
+  .clear-view-cache-button {
+    color: #ffffff;
+    background: #0a76a4;
+    border: none;
+    border-radius: 6px;
+    padding: 9px;
+    cursor: pointer;
+  }
+</style>
